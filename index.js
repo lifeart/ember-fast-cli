04d268e808fac209ab455344e66781fc9de21fd5
Aleksandr Kanunnikov <lifeart92@gmail.com>
2019-12-20 00:31:39 +0300

    unstable language server integraion

--- index.js
+++ index.js
@@ -3,12 +3,12 @@
 // eslint-disable-next-line node/no-unpublished-require
 const lookupCommand = require("ember-cli/lib/cli/lookup-command");
 // eslint-disable-next-line node/no-unpublished-require
-const serveURL = require('ember-cli/lib/utilities/get-serve-url');
-const UI_PATCH_ID = 'PATCH_9cf61e15-5685-4308-8938-e5c991825bc6';
+const serveURL = require("ember-cli/lib/utilities/get-serve-url");
+const UI_PATCH_ID = "PATCH_9cf61e15-5685-4308-8938-e5c991825bc6";
 const path = require("path");
-const fs = require('fs');
+const fs = require("fs");
 const express = require("express");
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 async function executeCommand(cli, commandName, commandArgs) {
   try {
     const command = makeCommand(cli, commandName, commandArgs);
@@ -57,25 +57,24 @@ let results = [];
 
 const methodsToPatch = [
   // 'write',
-  'writeError',
+  "writeError",
   // 'prependLine',
-  'writeDeprecateLine',
-  'writeLine', 
-  'writeErrorLine', 
-  'writeDebugLine', 
-  'writeInfoLine', 
-  'writeWarnLine'
+  "writeDeprecateLine",
+  "writeLine",
+  "writeErrorLine",
+  "writeDebugLine",
+  "writeInfoLine",
+  "writeWarnLine"
 ];
 
-function postData(url = '/', data = {}) {
+function postData(url = "/", data = {}) {
   return fetch(url, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json;charset=utf-8'
+      "Content-Type": "application/json;charset=utf-8"
     },
-    body: JSON.stringify({data})
-  })
-  .then(response => response.json());
+    body: JSON.stringify({ data })
+  }).then(response => response.json());
 }
 
 module.exports = {
@@ -83,22 +82,34 @@ module.exports = {
   // ember-language-server inegraion
   onInit(_, project) {
     // eslint-disable-next-line no-unused-vars
-    project.executors['els.executeInEmberCLI'] = async (_,__, [cmd] ) => {
+    project.executors["els.executeInEmberCLI"] = async (server, __, [cmd]) => {
       try {
-        let cliMetaFileName = path.join(__dirname, 'meta.json');
+        let cliMetaFileName = path.join(__dirname, "meta.json");
         if (!fs.existsSync(cliMetaFileName)) {
+          server.connection.window.showErrorMessage(
+            "Unable to find server params, try start ember server inside project directory"
+          );
           return;
         }
-        let meta = JSON.parse(fs.readFileSync(cliMetaFileName, 'utf8'));
+        let meta = JSON.parse(fs.readFileSync(cliMetaFileName, "utf8"));
         if (meta && meta.endpoint) {
-          await postData(meta.endpoint, (cmd||'').replace('ember ','').trim().split(' ').map(e=>e.trim()).filter(e=>e.length));
-          // meta
+          let results = await postData(
+            meta.endpoint,
+            (cmd || "")
+              .replace("ember ", "")
+              .trim()
+              .split(" ")
+              .map(e => e.trim())
+              .filter(e => e.length)
+          );
+          if (results.length) {
+            server.connection.window.showInformationMessage(results[0]);
+          }
         }
-      } catch(e) {
-        // 
+      } catch (e) {
+        server.connection.window.showErrorMessage(JSON.stringify(e));
       }
-    }
-    //  fs.writeFileSync(path.join(__dirname, 'meta.json'), JSON.stringify({endpoint}), 'utif');
+    };
   },
   serverMiddleware(config) {
     let app = config.app;
@@ -118,7 +129,11 @@ module.exports = {
       const [command, ...commandArgs] = req.body.data;
       executeCommand(this.parent.cli, command, commandArgs).then(() => {
         if (results.length === 0) {
-          results.push('No output, likely the specified command '+command+' is invalid. For available options, see `ember help`.');
+          results.push(
+            "No output, likely the specified command " +
+              command +
+              " is invalid. For available options, see `ember help`."
+          );
         }
         res.json(results);
         results = [];
@@ -128,22 +143,26 @@ module.exports = {
 
     let endpoint = `${serveURL(config.options, config.options.project)}cli`;
 
-    fs.writeFileSync(path.join(__dirname, 'meta.json'), JSON.stringify({endpoint}), 'utif');
-    this.ui.writeLine('');
+    fs.writeFileSync(
+      path.join(__dirname, "meta.json"),
+      JSON.stringify({ endpoint }),
+      "utf8"
+    );
+    this.ui.writeLine("");
     this.ui.writeLine(`[ember-fast-cli] Serving on: ${endpoint}`);
-    this.ui.writeLine('');
+    this.ui.writeLine("");
 
     if (UI_PATCH_ID in this.ui) {
       return;
     }
-    methodsToPatch.forEach((methodName)=>{
+    methodsToPatch.forEach(methodName => {
       let originalImplementation = this.ui[methodName];
       this.ui[methodName] = (...args) => {
         if (capturing) {
           results.push(args[0]);
         }
         originalImplementation.apply(this.ui, args);
-      }
+      };
     });
     this.ui[UI_PATCH_ID] = true;
   },
